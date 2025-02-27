bc07355de5d8c2529cbacb04e4e5dd6ffd97d7b4
Lori-L23 <atefalori@icloud.com>
2025-02-27 10:03:46 +0100

    6comit moidicstions

--- Formulaire.js
+++ Formulaire.js
@@ -14,11 +14,8 @@ if (email != "nomutilisateur@domaine"){
     valid=false;
 }
 
-// verifier le format des numéros de téléphone
-if (telephone.lenght > 9) {
-    document.getElementById("telephone").innerHTML = "<span style='color: red;'>Numero de telephone invalide, entre moins de 9 caractères.</span>";
-    valid = false;
-}
+
+
 
 // Limiter l’âge à une plage cohérente (ex. : 18-65 ans)
 if (age.length < 18 || age.length > 65) {
@@ -33,18 +30,20 @@ let duree = dateexperience - dateactuelle;
 if (dateexperience > dateactuelle) {
 
 }
-// S’assurer que la date de formation est valide 
+// S’assurer que la date de formation est valide
 
+// verifier le format des numéros de téléphone
+ function telephone(){
+    if (telephone.lenght > 9) {
+        document.getElementById("telephone").innerHTML = "<span style='color: red;'>Numero de telephone invalide, entre moins de 9 caractères.</span>";
+        valid = false;
+    }
+ }
 
-const bouton = document.getElementById('monbouton1');
-const liste = document.getElementById('listeEP').value;
-const fleche = bouton.querySelector('img'); // ou svg
-
-bouton.getElementById('monbouton1').addEventListener('click', () => {
-    liste.classList.toggle('hidden');
-    //exemple de rotation d'image
-    fleche.classList.toggle('rotate-180');
-});
-document.querySelector("#record-references > div.w-full.flex.relative.flex-col > div > div.w-full.mb-4 > div.-mx-2.flex.flex-col > div")
-
-document.querySelector("#record-languages > div.w-full.flex.relative.flex-col")
+ function age(){
+    // Limiter l’âge à une plage cohérente (ex. : 18-65 ans)
+if (age.length < 18 || age.length > 65) {
+    document.getElementById("age").innerHTML = "<span style='color: red;'>votre afes doit etre compris entre 18 et 65 ans</span>";
+    valid = false;
+}
+ }
